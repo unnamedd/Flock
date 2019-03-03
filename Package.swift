c7b5e555b4ec04478dffa8348f2ff6722328bd3a
Thiago Holanda <unnamedd@gmail.com>
2019-03-03 23:09:22 +0100

    Update framework to Swift 4.2

--- Package.swift
+++ Package.swift
@@ -1,11 +1,22 @@
-// swift-tools-version:3.2
-
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
-    name: "flock",
+    name: "FlockKit",
+    products: [
+        .library(
+            name: "FlockKit", 
+            targets: ["Flock"]
+        )
+    ],
     dependencies: [
-        .Package(url: "https://github.com/jpsim/SourceKitten", majorVersion: 0),
-        .Package(url: "https://github.com/kylef/PathKit", majorVersion: 0)
+        .package(url: "https://github.com/jpsim/SourceKitten", from: "0.22.0"),
+        .package(url: "https://github.com/kylef/PathKit", from: "0.9.2")
+    ],
+    targets: [
+        .target(
+            name: "Flock", 
+            dependencies: ["SourceKittenFramework", "PathKit"]
+        )
     ]
 )
